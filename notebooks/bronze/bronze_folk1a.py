cea626d002da9be4b2091e6ad8476550f1b60f01
OscarReseke <oreseke@gn.com>
2025-08-06 14:15:07 +0200

    Update bronze_folk1a.py to use semicolon as delimiter when reading CSV file.

--- notebooks/bronze/bronze_folk1a.py
+++ notebooks/bronze/bronze_folk1a.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 load_date = "2025-08-06"
 csv_path = f"/Volumes/ingest/default/raw_folk1a/ingest_date={load_date}/data.csv"
 
-df = spark.read.option("header", True).csv(csv_path)
+df = spark.read.option("header", True).option("delimiter", ";").csv(csv_path)
 
 df.printSchema()
 df.show(5)
